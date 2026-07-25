1851080604f1986f4a9224ea000b7ca0f20f3447
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-26 06:04:47 +0800

    Check this any number even or odd

---
+++ problem-01.js
@@ -0,0 +1,13 @@
+// check this any number even or odd
+
+
+function  evenOdd(number) {
+    if(number%2===0){
+        return "even"
+    }
+    else{
+        return "odd"
+    }
+}
+console.log(evenOdd(32));
+console.log(evenOdd(13));
