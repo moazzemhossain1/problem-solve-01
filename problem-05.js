8637c7bb459bcbb71746ccdffbafe3dddd165c8b
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-26 07:44:37 +0800

    Revers a string

---
+++ problem-05.js
@@ -0,0 +1,12 @@
+// problem -05: Revers a string 
+
+let str="progrmming";
+let revess="";
+for(i=str.length -1; i>=0;i--){
+   let valeu= str[i];
+//    console.log(valeu)
+
+    revess=revess+valeu;
+   
+}
+ console.log(revess)
