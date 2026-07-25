f9aa08ad274078bacead74214c143268be8defbd
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-26 06:31:47 +0800

    sum of range number 1 to .... n

---
+++ problem-02.js
@@ -0,0 +1,34 @@
+// 02 : sum of range of nember (1.......n);
+
+// let sum=0;
+// let number=7;
+// for(let i=1;i<=7;i++){
+//     sum=sum+i;
+// }
+
+// console.log("The totle sum is =",sum)
+
+
+
+
+//  using for loop 
+
+
+
+function sumNumber(number) {
+    // let number = 10;
+    //  console.log(number)     
+    let sum = 0;
+    for (let i = 1; i <= number; i++) {
+        sum = sum + i;
+    } return sum;
+}
+const result = (sumNumber(10));
+console.log("The totle numbers is =", result)
+
+
+
+
+
+
+
