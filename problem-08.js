644584d05aeedada403335b0b88d6aee60794c8f
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-26 09:04:56 +0800

    Count sentece

---
+++ problem-08.js
@@ -0,0 +1,14 @@
+// count word in a sentence;
+
+
+
+// let str="Bandgladens";
+// const splideArry=str.split("");
+// console.log(splideArry.length)
+
+function couunWord(str) {
+    const splideArry = str.split("");
+    return (splideArry.length)
+
+}
+console.log(couunWord("Bangladesh"))
