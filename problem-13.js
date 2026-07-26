77b15f258ef234bcf4ba7e37ab1e3b9f143f83c7
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-26 12:38:08 +0800

    loop thous in a object

---
+++ problem-13.js
@@ -0,0 +1,14 @@
+// porblem 13: Loop throu an objects proprites
+
+
+
+let students={
+    name:"Moazzem",
+    age:19,
+    rolle:842726,
+}
+
+for(let student in students){
+    console.log(students["name"])
+    
+}
